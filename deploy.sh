075ecb4bf136504b7e280ca30a2283d73a07f519
slorenzo <SebastianLorenzo@gmail.com>
2018-11-08 20:22:14 -0300

    Added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,57 @@
+# variables
+stage=${STAGE}
+region=${REGION}
+bucket=${BUCKET}
+secrets='/deploy/secrets/secrets.json'
+
+# Configure your Serverless installation to talk to your AWS account
+sls config credentials \
+  --provider aws \
+  --key ${AWS_KEY} \
+  --secret ${AWS_SECRET} \
+  --profile serverless-admin
+
+# cd into functions dir
+cd /deploy/functions
+
+# Deploy functions
+echo "------------------"
+echo 'Deploying function...'
+echo "------------------"
+sls deploy
+
+# find and replace the service endpoint
+if [ -z ${stage+dev} ]; then echo "Stage is unset."; else echo "Stage is set to '$stage'."; fi
+
+sls info -v | grep ServiceEndpoint > domain.txt
+sed -i 's@ServiceEndpoint:\ https:\/\/@@g' domain.txt
+sed -i "s@/$stage@@g" domain.txt
+domain=$(cat domain.txt)
+sed "s@.execute-api.$region.amazonaws.com@@g" domain.txt > id.txt
+id=$(cat id.txt)
+
+echo "------------------"
+echo "Domain:"
+echo "  $domain"
+echo "------------------"
+echo "API ID:"
+echo "  $id"
+
+rm domain.txt
+rm id.txt
+
+echo "{\"DOMAIN\":\"$domain\"}" > $secrets
+
+cd /deploy/bucket
+
+# Deploy bucket config
+echo "------------------"
+echo 'Deploying bucket...'
+sls deploy
+
+echo "------------------"
+echo 'Bucket endpoint:'
+echo "  http://$bucket.s3-website.$region.amazonaws.com/"
+
+echo "------------------"
+echo "Service deployed. Press CTRL+C to exit."
